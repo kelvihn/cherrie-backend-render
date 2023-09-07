7bd546dff6dc5d787332b5f0248977dffa8b0f31
Oghenetega Roy-Omoni <tmp11091@firstbanknigeria.com>
2023-09-07 15:53:35 +0100

    changed time to data to striung

--- server/chatTopic/chatTopic.controller.js
+++ server/chatTopic/chatTopic.controller.js
@@ -233,14 +233,14 @@ exports.getChatThumbList = async (req, res) => {
 
     const chatList_ = chatList[0].list.map((data) => ({
       ...data,
-      time:
-        now.diff(data.createdAt, "minute") <= -290 &&
-        now.diff(data.createdAt, "minute") >= -330
-          ? now.diff(data.createdAt, "minute") + 329 + " minutes ago"
-          : now.diff(data.createdAt, "hour") >= 24
-          ? dayjs(data.createdAt).format("DD MMM, YYYY")
-          : now.diff(data.createdAt, "hour") + 4 + " hour ago",
-      //time: data.createdAt
+      // time:
+      //   now.diff(data.createdAt, "minute") <= -290 &&
+      //   now.diff(data.createdAt, "minute") >= -330
+      //     ? now.diff(data.createdAt, "minute") + 329 + " minutes ago"
+      //     : now.diff(data.createdAt, "hour") >= 24
+      //     ? dayjs(data.createdAt).format("DD MMM, YYYY")
+      //     : now.diff(data.createdAt, "hour") + 4 + " hour ago",
+      time: data.createdAt.toString()
     }));
     console.log("fakeUserList", fakeUserList);
     console.log("chatList", chatList);
